321c4102ae3de14e710e19eee7d9b46f43ff104a
JinWoong Hong <slsl03264@naver.com>
2025-04-04 13:41:55 +0900

    Update 삼각 달팽이.java

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -3,10 +3,10 @@ class Solution {
     public int[] solution(int n) {
         int length = (n*(n+1))/2;
         answer = new int[length];
-        triangleSnail(0 , 0 ,n, 0);
+        triangleSnail(0 , 0 ,n);
         return answer;
     }
-    static public void triangleSnail(int start, int m, int n, int space) {
+    static public void triangleSnail(int start, int m, int n) {
         if (n <= 0) {
             return;
         }
@@ -19,6 +19,6 @@ class Solution {
         for (int i = n; i>2; i--) {
             answer[((i+m)*(i+m-1))/2 - 1 - m + m/2] = start + 3 * n - i;
         }
-        triangleSnail(start+ 3 * n - 3, m + 2, n - 3, 1);
+        triangleSnail(start+ 3 * n - 3, m + 2, n - 3);
     }
 }
