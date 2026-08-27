b7eeb7ed1b39bc9b013e2996808cbde9ad3ef478
JinWoong Hong <slsl03264@naver.com>
2026-08-27 18:06:18 +0900

    [level 3] Title: 정수 삼각형, Time: 13.34 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,23 +1,34 @@
+import java.util.ArrayList;
 class Solution {
+    static ArrayList<Integer> answer;
     public int solution(int[][] triangle) {
-        int answer = 0;
-        for (int i = 1; i< triangle.length; i++) {
-            for (int j = 0; j < triangle[i].length; j++) {
-                if (j == 0) {
-                    triangle[i][j] = triangle[i - 1][j] + triangle[i][j];
-                } else if (j < triangle[i].length - 1) {
-                    int num = triangle[i][j];
-                    triangle[i][j] = Math.max(triangle[i-1][j-1] + num , triangle[i-1][j] + num);
-                } else {
-                    triangle[i][j] = triangle[i][j] + triangle[i-1][j-1];
-                }
-            }
+        answer = new ArrayList<>();
+        answer.add(triangle[0][0]);
+        for (int i = 1; i < triangle.length; i++) {
+            dp(triangle[i]);
         }
         
-        for (int number : triangle[triangle.length - 1]) {
-            answer = answer > number ? answer : number;
-        }
+        int max= 0;
         
-        return answer;
+        for (int i = 0; i < triangle.length;i++) {
+            max = answer.get(i) > max ? answer.get(i) : max;
+        }
+        return max;
     }
+    
+    public void dp(int[] nextArray) {
+        ArrayList<Integer> nextAnswer = new ArrayList<>();
+        for (int i = 0; i < nextArray.length; i++) {
+            if (i == 0) {
+                nextAnswer.add(answer.get(0) + nextArray[0]);
+            } else if (i < nextArray.length - 1) {
+                nextAnswer.add(answer.get(i) > answer.get(i-1) ? answer.get(i) + nextArray[i] : answer.get(i-1) + nextArray[i]);
+            } else {
+                nextAnswer.add(answer.get(i - 1) + nextArray[i]);
+            }
+            
+        }
+        answer = nextAnswer;
+    }
+    
 }
